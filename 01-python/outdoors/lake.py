dd511a0da21fd48a418ba3e35babd31156037ede
Qing Han <qinghan@bu.edu>
2020-01-27 17:48:40 -0500

    add lake

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,3 +1,10 @@
-def draw_lake():
-    print("lake not found")
+def draw_lake(length=50, width=50, boat=True, x=2, y=2):
+    for i in range(length):
+        for j in range(width):
+            if(boat==True and x==j and y==i):
+                print("\____/", end = "")
+            else:
+                print("~", end = "")
+        print()
     return
+
